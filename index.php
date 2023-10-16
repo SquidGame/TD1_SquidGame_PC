ea94a9163e33ba979e0ec02d081f6ff7a8fae9d5
lepley <lepley.gaetan@etu.unicaen.fr>
2023-10-16 12:08:45 +0200

    project init

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/style.css">
+    <link rel="icon" href="./icons/Logo.png">
+    <title>Projet PHP - PtitCuisto</title>
+</head>
+<body>
+    <?php
+
+    ?>
+    <img alt="housmous-img" src="./images/houmous.jpg">
+</body>
+</html>
