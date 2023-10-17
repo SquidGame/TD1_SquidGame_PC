ef4deab7acbfbccc94518d916e9d64cd18192e30
lepley221 <gaetan.lepley@etu.unicaen.fr>
2023-10-17 20:43:53 +0200

    accueil finit

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css">
+</head>
+
+<header class="bg-blue-500 p-4 flex justify-between items-center">
+    <div class="flex items-center space-x-1">
+        <img src="../images/logo.png" alt="Logo" class="w-25 h-10">
+    </div>
+    <div class="flex space-x-4">
+        <a href="#" class="text-white">Accueil</a>
+        <a href="#" class="text-white">Nos recettes</a>
+        <div class="relative group">
+            <a href="#" class="text-white">Filtres</a>
+            <ul class="hidden absolute left-0 mt-2 space-y-1 bg-white text-gray-700 group-hover:block">
+                <li><a href="#">Catégorie</a></li>
+                <li><a href="#">Titre</a></li>
+                <li><a href="#">Ingrédient</a></li>
+            </ul>
+        </div>
+        <a href="#" class="text-white">Connexion</a>
+    </div>
+</header>
+
--- index.php
+++ index.php
@@ -1,13 +1,15 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="/dist/output.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PtitCuisto - PHP Project</title>
 </head>
 <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <?php
+        require_once("./includes/header.php");
+        require_once("./includes/body.php");
+        require_once("./includes/footer.php");
+    ?>
 </body>
 </html>
