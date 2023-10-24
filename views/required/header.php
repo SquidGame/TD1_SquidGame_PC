1053a3f8659664e4d5d74591f50edc2bfbb64cf6
Ianis Le Blay <leblay223@campus.unicaen.fr>
2023-10-24 11:35:37 +0200

    Amelioration du css de la navbar + correction de bug sur la navbar

--- views/required/header.php
+++ views/required/header.php
@@ -1,27 +1,31 @@
-<nav>
+<nav class="bg-[#7890cd] flex flex-wrap	items-center justify-between ">
     <link rel="stylesheet" href="../styles/header-style.css">
     <div class="logo">
-        <img class="image-navbar" alt="logo PtitCuisto" src="../images/logo.png">
+        <img class="image-navbar cursor-pointer	" alt="logo PtitCuisto" src="../images/logo.png">
     </div>
-    <div class="nav-items">
-        <li><a href="../index.php">Accueil</a></li>
-        <li><a href="#">Nos recettes</a></li>
-        <li class="submenu-parent">
-            <a href="#">Filtres</a>
-            <ul class="submenu">
-                <li><a href="#">Catégorie</a></li>
-                <li><a href="#">Titre</a></li>
-                <li><a href="#">Ingrédient</a></li>
+    <div class="nav-items flex flex-1 justify-end list-none text-[#fff] ">
+        <li><a class="hover:text-[#ff3d00]" href="../index.php">Accueil</a></li>
+        <li><a class="hover:text-[#ff3d00]" href="#">Nos recettes</a></li>
+        <li class="submenu-parent relative">
+            <a class="hover:text-[#ff3d00]" href="#">
+                <span class="filter-text">Filtres</span>
+                <span id="fleche" class="arrow-down fas fa-angle-down"></span>
+            </a>
+            <ul class="submenu" id="submenu">
+                <li><a class="hover:text-[#ff3d00]" href="#">Catégorie</a></li>
+                <li><a class="hover:text-[#ff3d00]" href="#">Titre</a></li>
+                <li><a class="hover:text-[#ff3d00]" href="#">Ingrédient</a></li>
             </ul>
         </li>
-        <li><a href="./views/login.php">Connexion</a></li>
+
+
+        <li><a class="hover:text-[#ff3d00]" href="./views/login.php">Connexion</a></li>
     </div>
-    <div class="cancel-icon">
+    <div class="cancel-icon text-center text-[#fff] cursor-pointer">
         <span class="fas fa-times"></span>
     </div>
-    <div class="menu-icon">
+    <div class="menu-icon text-center text-[#fff] cursor-pointer">
         <span class="fas fa-bars"></span>
     </div>
     <script src="../app/header.js"></script>
 </nav>
-
